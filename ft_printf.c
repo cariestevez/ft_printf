7b1cb0d7528200f99d6b41955ddfc37a54c0d97f
cariestevez <carinaestevezorth@gmail.com>
2023-01-14 02:30:48 +0100

    almost done

--- ft_printf.c
+++ ft_printf.c
@@ -6,103 +6,172 @@
 /*   By: cestevez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 12:24:01 by cestevez          #+#    #+#             */
-/*   Updated: 2023/01/12 23:32:24 by cestevez         ###   ########.fr       */
+/*   Updated: 2023/01/14 02:27:09 by cestevez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
-#include "libft.h"
+#include <unistd.h>
+#include <stdio.h>
+//#include "libft.h"
 
-/*	1st % -- 1st additional arg
-	2nd % -- 2nd add arg
-	etc
-	% followed by a letter that specifies the format
-*/
-
-int	format_check(const char *c, const char *to_print)
-{
-	if (c == c)
-	{
-		ft_putchar(to_print);
-		return (ft_putchar);
-	}
-/*	if (format == s)
-	{
-		
-	}
-//d takes as much space as needed
-	if (format == d)
-	{
-
-	}
-	if (format == i)
-	{
-
-	}
-	if (format == %)
-	{
-
-	}
-	if (format == p)
-	{
-
-	}
-	if (format == u)
-	{
-
-	}
-	if (format == x)
-	{
-
-	}
-	if (format == X)
-	{
-
-	}
-*/	
-}
+int		unsigned_printf(unsigned int n);
+char	*ft_strchr(const char *s, int c);
+int		putnbr_printf(int n);
+int		putstr_printf(char *s);
+int		putchar_printf(char c);
+int		type_print(const char *c, va_list arg);
 
 int	ft_printf(const char *format, ...)
 {
 	va_list		arg_list;
-	const char	*argument;
-	size_t		i;
-	size_t		j;
-	size_t		format_len;
+	char		*type_ptr;
+	int			i;
+	int			len;
 
 	i = 0;
-	j = 0;
-//get the length of the ppal str
-	format_len = ft_strlen(format);
+	len = 0;
+	if (format == 0)
+		return (0);
 //initialize the arg list
 	va_start(arg_list, format);
-//while ?? ,get the next arg. va_arg dereferences the ptr-->to get access
-//to the value inside of the variable it points to, and advances the pointer
-//to skip past that argument
-	while(*format != 0)
+//no need to get the next arg for now, just iterate the 1st str
+//to print it + the other args (if there)
+	while (format[i] != 0)
 	{
-		if (*format == "%" && *(format + 1) == "%")
-			ft_putchar(*(format + 1));
-		while (*format != "%")
-			ft_putstr(*format);
-//does the va_list recognize from itself where the argument begins 
-//or do I have to give a return with the position?
-		if (*format == "%")
+		type_ptr = ft_strchr("cspdiuxX%", format[i + 1]);
+		if (format[i] == '%' && type_ptr != 0)
 		{
-			argument = va_arg(arg_list, format);
-			format_check(*(format + 1), argument);
+			len += type_print(type_ptr, arg_list);
+			i++;
 		}
-		
-		
-		
+		else
+			len += write(1, &format[i], 1);
+		i++;
 	}
-	va_end(args);
+	va_end(arg_list);
+	return (len);
+}
+//Man says: If arg_list is passed to a function that uses va_arg(arg_list,type),
+//then the value of arg_list is undefined after the return of that function!!!
+//use va_copy to avoid undefined values??!
 
+int	type_print(const char *c, va_list arg_list)
+{
+	int	len;
+//va_arg dereferences the ptr-->to get access to the value inside
+//of the variable it points to, and advances the pointer automatically
+//each time we call it to skip past that argument. How? no idea
+//as arg we give the list of args in which to iterate and the type we
+//want it to return??
+	len = 0;
+	if (*c == '%')
+		len += write(1, "%", 1);
+	if (*c == 'c')
+		len = putchar_printf(va_arg(arg_list, int));
+	if (*c == 's')
+		len = putstr_printf(va_arg(arg_list, char *));
+//d takes as much space as needed
+//with printf both can be printed the same way, but with scanf they
+//have to be treated different
+	if (*c == 'd' || *c == 'i')
+		len = putnbr_printf(va_arg(arg_list, int));
+	if (*c == 'u')
+		len = unsigned_printf(va_arg(arg_list, unsigned int));
+/*	if (c == 'x')
+	{
+
+	}
+	if (c == 'X')
+	{
+
+	}
+	if (c == 'p')
+	{
+
+	}*/
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)&s[i]);
+	return (0);
+}
+
+int	putchar_printf(char c)
+{
+	int	len;
+
+	len = write (1, &c, 1);
+	return (len);
+}
+
+int	putstr_printf(char *s)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (s[i] != 0)
+	{
+		len += write (1, &s[i], 1);
+		i++;
+	}
+	return (len);
+}
+
+int	putnbr_printf(int n)
+{
+	int	putme;
+	int	len;
+
+	len = 0;
+	if (n == -2147483648)
+		len = write (1, "-2147483648", 11);
+	else
+	{
+		if (n < 0)
+		{
+			n *= -1;
+			len += write (1, "-", 1);
+		}
+		if (n > 9)
+			putnbr_printf(n / 10);
+		putme = n % 10 + '0';
+		len += write(1, &putme, 1);
+	}
+	return (len);
+}
+
+int	unsigned_printf(unsigned int n)
+{
+	int	putme;
+	int	len;
+
+	len = 0;
+	if (n > 9)
+		putnbr_printf(n / 10);
+	putme = n % 10 + '0';
+	len += write(1, &putme, 1);
+	return (len);
 }
 
 int	main()
 {
-	ft_printf("Something like this %d, %s", 10, "string");
-	printf("Something like this %d, %s", 10, "string");
+	ft_printf("Something like %u", -10);
+	printf("\n");
+	printf("Something like %u", -10);
 	return (0);
 }

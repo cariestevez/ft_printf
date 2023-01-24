5e7360ba8cc007f633671ff6d6bde682586cb357
cariestevez <116964367+cariestevez@users.noreply.github.com>
2023-01-24 14:35:20 +0100

    some final retouches and tests left

--- ft_printf.c
+++ ft_printf.c
@@ -16,24 +16,26 @@
 #include <stdlib.h>
 //#include "libft.h"
 
-char 		*tolower_printf(char *s);
-char		*hex_convert(unsigned num, int base, const char *c);
-char		*convert_address(size_t num, int base);
-char		*ft_strchr(const char *s, int c);
-int			putnbr_printf(int n, const char *c);
-int			putstr_printf(char *s);
-int			putchar_printf(char c);
-int			type_print(const char *c, va_list arg);
+char	*toupper_printf(char *s);
+char	*printf_hex(unsigned num, int base, const char *c);
+char	*hex_ptr(char *ptr, int i, int j, unsigned num, int base);
+char	*printf_address(size_t num, int base);
+char	*ft_strchr(const char *s, int c);
+int		putnbr_printf(int n, int len);
+int		putunsigned_printf(unsigned n, const char *c, int len);
+int		putstr_printf(char *s);
+int		putchar_printf(char c);
+int		type_print(const char *c, va_list arg);
 
 int	ft_printf(const char *format, ...)
 {
 	va_list		arg_list;
 	char		*type_ptr;
 	int			len;
-    int         i;
+	int         i;
 
 	len = 0;
-    i = -1;
+	i = -1;
 	if (format == 0)
 		return (0);
 	va_start(arg_list, format);
@@ -41,8 +43,7 @@ int	ft_printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			type_ptr = ft_strchr("cspdiuxX%", format[i++ + 1]);
-			if (type_ptr == 0)
+			if (!(type_ptr = ft_strchr("cspdiuxX%", format[i++ + 1])))
 				return (0);
 			len += type_print(type_ptr, arg_list);
 		}
@@ -63,27 +64,20 @@ int	type_print(const char *c, va_list arg_list)
 		return (0);
 	if (*c == '%')
 		len += write(1, "%", 1);
-	if (*c == 'c')
+	if (*c == 'c' || *c == '%')
 		len = putchar_printf(va_arg(arg_list, int));
 	if (*c == 's')
 		len = putstr_printf(va_arg(arg_list, char *));
 	if (*c == 'd' || *c == 'i')
-		len = putnbr_printf(va_arg(arg_list, int), c);
+		len = putnbr_printf(va_arg(arg_list, int), len);
 	if (*c == 'u')
-		len = putnbr_printf(va_arg(arg_list, unsigned int), c);
+		len = putunsigned_printf(va_arg(arg_list, unsigned int), c, len);
 	if (*c == 'x' || *c == 'X')
-	{
-		converted = hex_convert(va_arg(arg_list, unsigned int), 16, c);
-		len = putstr_printf(converted);
-
-	}
+		converted = printf_hex(va_arg(arg_list, unsigned int), 16, c);
 	if (*c == 'p')
-	{
-		converted = convert_address(va_arg(arg_list, size_t), 16);
+		converted = printf_address(va_arg(arg_list, size_t), 16);
+	if (*c == 'x' || *c == 'X' || *c == 'p')
 		len = putstr_printf(converted);
-
-	}
-	
 	return (len);
 }
 
@@ -128,36 +122,41 @@ int	putstr_printf(char *s)
 	return (len);
 }
 
-int	putnbr_printf(int n, const char *c)
+int	putnbr_printf(int n, int len)
 {
-	int	putme;
-	int	len;
+	char	num;
 
-	len = 0;
-	if (n == -2147483648 && *c != 'u')
-		len = write (1, "-2147483648", 11);
-	else
+	if (n > -10 && n < 0)
+		len += write(1, "-", 1);
+	if (n >= 10 || n <= -10)
+		putnbr_printf(n / 10, len);
+	if (n < 0)
 	{
-		if (n < 0 && *c != 'u')
-		{
-			n *= -1;
-			len += write (1, "-", 1);
-		}
-		if (n > 9)
-			putnbr_printf(n / 10, c);
-		putme = n % 10 + 48;
-		len += write(1, &putme, 1);
+		n %= 10;
+		n *= -1;
 	}
+	num = (n % 10) + 48;
+	len += write(1, &num, 1);
 	return (len);
 }
 
-char* hex_convert(unsigned num, int base, const char *c)
+int	putunsigned_printf(unsigned n, const char *c, int len)
 {
-	char	str[] = "0123456789ABCDEF";
-	char	*ptr;
-	int		temp;
-	int		i;
-	int		j;
+	char	num;
+
+	if (n >= 10)
+		putunsigned_printf(n / 10, c, len);
+	num = (n % 10) + 48;
+	len = write(1, &num, 1);
+	return (len);
+}
+
+char	*printf_hex(unsigned num, int base, const char *c)
+{
+	char		*ptr;
+	unsigned	temp;
+	int			i;
+	int			j;
 
 	j = 0;
 	i = 0;
@@ -171,6 +170,16 @@ char* hex_convert(unsigned num, int base, const char *c)
 		j = 1;
 	if (!(ptr = (char *)malloc(sizeof(char) * (i + j) + 1)))
 		return (0);
+	ptr = hex_ptr(ptr, i, j, num, base);
+	if (*c == 'X')
+		ptr = toupper_printf(ptr);
+	return (ptr);
+}
+
+char	*hex_ptr(char *ptr, int i, int j, unsigned num, int base)
+{
+	char	str[] = "0123456789abcdef";
+
 	if (num == 0)
 	{
 		ptr[j] = 0;
@@ -186,68 +195,68 @@ char* hex_convert(unsigned num, int base, const char *c)
 			i--;
 		}
 	}
-	if (*c == 'x')
-		ptr = tolower_printf(ptr);
 	return (ptr);
 }
 
-char* convert_address(size_t num, int base)
+char	*printf_address(size_t num, int base)
 {
 	char	str[] = "0123456789abcdef";
 	char	*ptr;
-	size_t		temp;
+	size_t	temp;
 	int		i;
 
-	i = 2;
+	i = 1;
 	temp = num;
 	while (temp != 0)
 	{
 		i++;
 		temp /= base;
-		//printf("%d")
 	}
 	if (!(ptr = (char *)malloc(sizeof(char) * i + 1)))
 		return (0);
 	ptr[i + 1] = 0;
-	ptr[1] = 'x';
-	ptr[0] = '0';
 	while (i > 1)
 	{
-		ptr[i]= str[num % base];
+		ptr[i--]= str[num % base];
 		num /= base;
-		i--;
 	}
+	ptr[1] = 'x';
+	ptr[0] = '0';
 	return (ptr);
 }
 
-char *tolower_printf(char *s)
+char	*toupper_printf(char *s)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (s[i] != 0)
-    {
-        if (s[i] > 64 && s[i] < 91)
-            s[i] += 32;
-        i++;
-    }
-    return (s);
+	i = 0;
+	while (s[i] != 0)
+	{
+		if (s[i] > 96 && s[i] < 123)
+			s[i] += 32;
+		i++;
+	}
+	return (s);
 }
-
-
-int	main()
+/*int	main()
 {
 	int	a = 2;
 	int *p = &a;
 
+	ft_printf("Something like %%\n");
+	printf("to be:Something like %%\n");
 	ft_printf("Something like %p\n", &a);
+	printf("Something like %p\n", &a);
 	ft_printf("Something like %p\n", p);
-	printf("to be:Something like %p\n", &a);
-	printf("to be:Something like %p\n", p);
+	printf("Something like %p\n", p);
 	ft_printf("Something like %x\n", 230837698);
 	printf("to be: Something like %x\n", 230837698);
-/*	printf(0);
-	ft_printf(0);*/
+	printf(0);
+	ft_printf(0);
+	ft_printf("Something like %u\n", -4294967294);
+	printf("to be: Something like %u\n", -4294967294);
+	ft_printf("Something like %i\n", 2147483648);
+	printf("to be: Something like %i\n", 2147483648);
 	return (0);
-}
+}*/
 

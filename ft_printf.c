ed4da0801a7fb99e9c248eca4ffc6c317d14e766
cariestevez <116964367+cariestevez@users.noreply.github.com>
2023-01-31 00:57:15 +0100

    Ready to go :D

--- ft_printf.c
+++ ft_printf.c
@@ -6,17 +6,11 @@
 /*   By: cestevez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 12:24:01 by cestevez          #+#    #+#             */
-/*   Updated: 2023/01/26 19:49:37 by cestevez         ###   ########.fr       */
+/*   Updated: 2023/01/31 00:53:51 by cestevez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-//#include "libft.h"
 
 int	type_print(const char c, va_list arg_list);
 
@@ -28,7 +22,6 @@ int	ft_printf(const char *format, ...)
 
 	len = 0;
 	i = 0;
-
 	va_start(arg_list, format);
 	while (format[i] != 0)
 	{
@@ -54,7 +47,7 @@ int	type_print(const char c, va_list arg_list)
 	if (c == 'c')
 		return (print_char(va_arg(arg_list, int)));
 	if (c == 's')
-		return (print_string(va_arg(arg_list, char *)));
+		return (print_string(va_arg(arg_list, char *), c));
 	if (c == 'd' || c == 'i')
 		return (print_nbr(va_arg(arg_list, int)));
 	if (c == 'u')
@@ -64,34 +57,44 @@ int	type_print(const char c, va_list arg_list)
 	if (c == 'p')
 		converted = print_address(va_arg(arg_list, size_t));
 	if (c == 'x' || c == 'X' || c == 'p')
-		return (print_string(converted));
+		return (print_string(converted, c));
 	else
 		return (-1);
 }
 
-int	main()
-{
-	int a;
-/*	int	a = 2;
-	int *p = &a;
-	ft_printf("Something like %%\n");
-	printf("to be:Something like %%\n");
-	ft_printf("Something like %p\n", &a);
-	printf("Something like %p\n", &a);
-	ft_printf("Something like %p\n", p);
-	printf("Something like %p\n", p);
-	ft_printf("Something like %x\n", 230837698);
-	printf("to be: Something like %x\n", 230837698);
-	printf(0);
-	ft_printf(0);*/
-	a = ft_printf("Something like %u\n", 967294);
-	printf("to be: Something like %u\n", 967294);
-	printf("a = %i\n", a);
-	a = ft_printf("Something like %i\n", 147483648);
-	printf("to be: Something like %i\n", 147483648);
-	printf("a = %i\n", a);
-	a = ft_printf("Something like %d\n", 10);
-	printf("to be: Something like %d\n", 10);
-	printf("a = %i\n", a);
-	return (0);
-}
+// int	main()
+// {
+// 	// int a;
+// 	// int	a = 2;
+// 	// int *p = &a;
+// 	// ft_printf("Something like %%\n");
+// 	// printf("to be:Something like %%\n");
+// 	// ft_printf("Something like %p\n", &a);
+// 	// printf("Something like %p\n", &a);
+// 	// ft_printf("Something like %p\n", p);
+// 	// printf("Something like %p\n", p);
+// 	// ft_printf("Something like %x\n", 230837698);
+// 	// printf("to be: Something like %x\n", 230837698);
+// 	// printf(0);
+// 	// ft_printf(0);
+// 	// a = ft_printf("Something like %u\n", 967294);
+// 	// printf("to be: Something like %u\n", 967294);
+// 	// printf("a = %i\n", a);
+// 	// a = ft_printf("Something like %i\n", 147483648);
+// 	// printf("to be: Something like %i\n", 147483648);
+// 	// printf("a = %i\n", a);
+// 	// a = ft_printf("Something like %d\n", 10);
+// 	// printf("to be: Something like %d\n", 10);
+// 	// printf("a = %i\n", a);
+// 	// printf(" %s %s %s %s %s ", " - ", "", "4", "", "2 ");
+// 	// printf("\n");
+// 	// ft_printf(" %s %s %s %s %s ", " - ", "", "4", "", "2 ");
+// 	// printf("\n");
+// 	// printf(" NULL %s NULL ", NULL);
+// 	// printf("\n");
+// 	// ft_printf(" NULL %s NULL ", NULL);
+// 	// printf(" %p %p ", 0, 0);
+// 	// printf("\n");
+// 	ft_printf(" hello %p ", 1);
+// 	return (0);
+// }
--- print_address.c
+++ print_address.c
@@ -6,37 +6,47 @@
 /*   By: cestevez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 23:10:11 by cestevez          #+#    #+#             */
-/*   Updated: 2023/01/25 01:00:46 by cestevez         ###   ########.fr       */
+/*   Updated: 2023/01/31 00:51:50 by cestevez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int	count_places(size_t temp);
+
 char	*print_address(size_t num)
 {
 	char	*str;
 	char	*ptr;
-	size_t	temp;
 	int		i;
 
 	str = "0123456789abcdef";
-	i = 1;
-	temp = num;
-	while (temp != 0)
-	{
-		i++;
-		temp /= 16;
-	}
+	i = 2 + count_places(num);
+	if (num == 0)
+		return (0);
 	ptr = (char *)malloc((sizeof(char)) * i + 1);
 	if (ptr == 0)
 		return (0);
-	ptr[i + 1] = 0;
-	while (i > 1)
+	ptr[i] = 0;
+	while (--i > 1)
 	{
-		ptr[i--] = str[num % 16];
+		ptr[i] = str[num % 16];
 		num /= 16;
 	}
 	ptr[1] = 'x';
 	ptr[0] = '0';
 	return (ptr);
+}
+
+int	count_places(size_t temp)
+{
+	int	i;
+
+	i = 0;
+	while (temp != 0)
+	{
+		i++;
+		temp /= 16;
+	}
+	return (i);
 }
